94aaca71c826c38782d0530dfd4080961b04d781
Moamin <moamin.salamah@gmail.com>
2023-04-12 11:48:15 +0200

    meal item component

--- recipes-app/src/views/MealsByName.vue
+++ recipes-app/src/views/MealsByName.vue
@@ -10,28 +10,7 @@
       />
     </div>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-3 p-8">
-      <div
-        v-for="meal of meals"
-        :key="meal.idMeal"
-        class="bg-white shadow rounded-xl"
-      >
-        <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
-          <img
-            :src="`${meal.strMealThumb}`"
-            :alt="meal.strMeal"
-            class="rounded-t-xl w-full h-48 object-cover"
-          />
-        </router-link>
-        <div class="p-3">
-          <h3 class="font-semibold">{{ meal.strMeal }}</h3>
-          <p class="mb-4 h-40 overflow-scroll">
-            {{ meal.strInstructions }}
-          </p>
-          <div class="flex items-center justify-between">
-            <YouTubeButton :href="meal.strYoutube">Youtube</YouTubeButton>
-          </div>
-        </div>
-      </div>
+      <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
     </div>
   </div>
 </template>
@@ -40,7 +19,7 @@
 import { ref, computed, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import store from "../store";
-import YouTubeButton from "../components/YouTubeButton.vue";
+import MealItem from "../components/MealItem.vue";
 
 const keyword = ref("");
 const meals = computed(() => store.state.searchedMeals);
