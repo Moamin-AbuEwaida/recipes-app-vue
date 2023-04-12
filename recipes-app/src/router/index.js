7765e4c1eca98b1332f60b930abe743f2ee1ade0
Moamin <moamin.salamah@gmail.com>
2023-04-12 09:43:26 +0200

    meal details page

--- recipes-app/src/router/index.js
+++ recipes-app/src/router/index.js
@@ -5,6 +5,8 @@ import Home from '../views/Home.vue'
 import MealsByName from '../views/MealsByName.vue'
 import MealsByLetter from '../views/MealsByLetter.vue'
 import MealsByIngredient from '../views/MealsByIngredient.vue'
+import MealDetails from '../views/MealDetails.vue'
+
 const routes = [
     {
         path: '/',
@@ -29,6 +31,11 @@ const routes = [
                 path: '/by-letter/:letter?',
                 name: 'byLetter',
                 component: MealsByLetter
+            },
+            {
+                path: '/meal/:id?',
+                name: 'mealDetails',
+                component: MealDetails
             },
         ]
     },
