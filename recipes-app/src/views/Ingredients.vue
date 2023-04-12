6dc1be2952e44587a08c915d65ca069a38a6e756
Moamin <moamin.salamah@gmail.com>
2023-04-12 14:43:50 +0200

    searching in ingredients page

--- recipes-app/src/views/Ingredients.vue
+++ recipes-app/src/views/Ingredients.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="p-8">
     <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 bg-white border-gray-200 w-full mb-3"
+      placeholder="Search for ingredient"
+    />
     <router-link
       :to="{
         name: 'byIngredient',
         params: { ingredient: ingredient.strIngredient },
       }"
-      v-for="ingredient of ingredients"
+      v-for="ingredient of computedIngredients"
       :key="ingredient.idIngredient"
       class="block bg-white rounded p-3 mb-3 shadow"
     >
@@ -19,10 +25,18 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import axiosClient from "../axiosClient";
 
 const ingredients = ref([]);
+const keyword = ref([]);
+
+const computedIngredients = computed(() => {
+  if (!computedIngredients) return ingredients;
+  return ingredients.value.filter((i) =>
+    i.strIngredient.toLowerCase().includes(keyword.value)
+  );
+});
 
 onMounted(() => {
   axiosClient.get("list.php?i=list").then(({ data }) => {
