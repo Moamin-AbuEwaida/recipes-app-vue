c603baa96ad7777971210edfb45d40bca2612678
Moamin <moamin.salamah@gmail.com>
2023-04-12 15:54:02 +0200

    home page

--- recipes-app/src/views/Home.vue
+++ recipes-app/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="flex flex-col p-8"></div>
+  <div>
+    <div class="p-8 pb-0">
+      <h1 class="text-4xl font-bold mb-4">Random Meals</h1>
+    </div>
+    <Meals :meals="meals" />
+  </div>
 </template>
 
 <script setup>
 import { computed, onMounted, ref } from "vue";
-import axiosClient from "../axiosClient";
-
-const ingredients = ref([]);
-
+import store from "../store";
+import Meals from "../components/Meals.vue";
+import axiosClient from "../axiosClient.js";
+const meals = ref([]);
 onMounted(async () => {
-  const response = await axiosClient.get("/list.php?i=list");
-
-  ingredients.value = response.data;
+  for (let i = 0; i < 10; i++) {
+    axiosClient
+      .get(`random.php`)
+      .then(({ data }) => meals.value.push(data.meals[0]));
+  }
 });
 </script>
-
-<style></style>
